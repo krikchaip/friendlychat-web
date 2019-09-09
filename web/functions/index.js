a2cdfbf4fd91102f31de9ede1bf65f7251214ae9
krikchaip <krikchai.p@gmail.com>
2019-09-09 17:57:25 +0700

    image blur function

--- web/functions/index.js
+++ web/functions/index.js
@@ -14,12 +14,21 @@
  * limitations under the License.
  */
 
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const { spawn } = require('child-process-promise');
+
 // Import the Firebase SDK for Google Cloud Functions.
 const functions = require('firebase-functions');
 
 // Import and initialize the Firebase Admin SDK.
 const firebase = require('firebase-admin');
 firebase.initializeApp();
+
+// Google Cloud Vision Client Library
+const Vision = require('@google-cloud/vision');
+const vision = new Vision();
 
 // Adds a message that welcomes new users into the chat.
 // ! https://stackoverflow.com/questions/57576674/firebase-trigger-function-ignored-due-to-missing-firebaseauth-googleapis-com-em
@@ -40,6 +49,52 @@ exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
   console.log('Welcome message written to database.');
 });
 
-// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
+// Checks if uploaded images are flagged as Adult or Violence and if so blurs them.
+exports.blurOffensiveImages =
+  functions.runWith({ memory: '2GB' }).storage.object().onFinalize(async object => {
+    const image = {
+      source: { imageUri: `gs://${object.bucket}/${object.name}` },
+    };
+
+    // Check the image content using the Cloud Vision API.
+    const batchAnnotateImagesResponse = await vision.safeSearchDetection(image);
+    const safeSearchResult = batchAnnotateImagesResponse[0].safeSearchAnnotation;
+    const Likelihood = Vision.types.Likelihood;
+
+    if (Likelihood[safeSearchResult.adult] >= Likelihood.LIKELY ||
+        Likelihood[safeSearchResult.violence] >= Likelihood.LIKELY) {
+      console.log('The image', object.name, 'has been detected as inappropriate.');
+      return blurImage(object.name);
+    }
+
+    console.log('The image', object.name, 'has been detected as OK.');
+  });
 
 // TODO(DEVELOPER): Write the sendNotifications Function here.
+
+// Blurs the given image located in the given bucket using ImageMagick.
+async function blurImage(filePath) {
+  const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
+  const messageId = filePath.split(path.sep)[1];
+  const bucket = firebase.storage().bucket();
+
+  // Download file from bucket.
+  await bucket.file(filePath).download({destination: tempLocalFile});
+  console.log('Image has been downloaded to', tempLocalFile);
+
+  // Blur the image using ImageMagick.
+  await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
+  console.log('Image has been blurred');
+
+  // Uploading the Blurred image back into the bucket.
+  await bucket.upload(tempLocalFile, {destination: filePath});
+  console.log('Blurred image has been uploaded to', filePath);
+
+  // Deleting the local file to free up disk space.
+  fs.unlinkSync(tempLocalFile);
+  console.log('Deleted local file.');
+
+  // Indicate that the message has been moderated.
+  await firebase.firestore().collection('messages').doc(messageId).update({moderated: true});
+  console.log('Marked the image as moderated in the database.');
+}
